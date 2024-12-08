0227c11edd71d2ee0ca0cf46ee0fbb5e35f1d3c8
sumihiro3 <sumihiro@gmail.com>
2024-12-08 15:11:03 +0900

    Lambda へ backend をデプロイ

--- apps/infra/bin/infra.ts
+++ apps/infra/bin/infra.ts
@@ -3,7 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import { TechPostCastInfraStack } from '../lib/infra-stack';
 
 const app = new cdk.App();
-new TechPostCastInfraStack(app, 'TechPostCastInfraStack', {
+new TechPostCastInfraStack(app, 'TechPostCastStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- apps/infra/lib/infra-stack.ts
+++ apps/infra/lib/infra-stack.ts
@@ -1,23 +1,46 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apiGatewayV2Integration from "aws-cdk-lib/aws-apigatewayv2-integrations";
+import * as apiGatewayV2 from "aws-cdk-lib/aws-apigatewayv2";
+import * as cloudWatchLogs from "aws-cdk-lib/aws-logs";
 import * as path from 'path';
+import { Platform } from 'aws-cdk-lib/aws-ecr-assets';
 
-const lambdaDir = path.join('../backend');
+const dockerfileDir = path.join(__dirname, '../../..');
 
 export class TechPostCastInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    new lambda.DockerImageFunction(this, 'TechPostCastLambda', {
-      code: lambda.DockerImageCode.fromImageAsset(lambdaDir),
+    // Lambda with Docker Image
+    const backendLambda = new lambda.DockerImageFunction(this, 'TechPostCastBackendLambda', {
+      code: lambda.DockerImageCode.fromImageAsset(dockerfileDir, {
+        platform: Platform.LINUX_AMD64,
+      }),
+        functionName: 'TechPostCastBackendLambda',
+        memorySize: 128,
+        timeout: cdk.Duration.seconds(10 * 60),
+        logRetention: cloudWatchLogs.RetentionDays.ONE_WEEK,
+      });
+    new cdk.CfnOutput(this, 'TechPostCastBackendLambdaArn', {
+      value: backendLambda.functionArn,
+      exportName: 'TechPostCastBackendLambdaArn',
+    });
+    new cdk.CfnOutput(this, 'TechPostCastBackendLambdaName', {
+      value: backendLambda.functionName,
+      exportName: 'TechPostCastBackendLambdaName',
     });
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // API Gateway
+    const backendGateway = new apiGatewayV2.HttpApi(this, 'TechPostCastBackendApi', {
+      defaultIntegration: new apiGatewayV2Integration.HttpLambdaIntegration(
+        `TechPostCastBackendLambdaIntegration`, backendLambda,
+      ),
+      apiName: `TechPostCastBackendApi`,
+    });
+    new cdk.CfnOutput(this, 'TechPostCastBackendApiUrl', {
+      value: backendGateway.url ?? 'NO URL',
+      exportName: 'TechPostCastBackendApiUrl',
+    });
   }
 }
