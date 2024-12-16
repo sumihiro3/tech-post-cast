6cc9a22f942c9d4b0e99b47afabc8074d0b0152e
sumihiro3 <sumihiro@gmail.com>
2024-12-16 17:11:39 +0900

    番組での紹介記事をメッセージで返せるようにした

--- apps/line-bot-backend/src/infrastructure/database/headline-topic-programs/headline-topic-programs.repository.ts
+++ apps/line-bot-backend/src/infrastructure/database/headline-topic-programs/headline-topic-programs.repository.ts
@@ -1,7 +1,9 @@
-import { HeadlineTopicProgram } from '.prisma/client';
 import { IHeadlineTopicProgramsRepository } from '@domains/headline-topic-programs/headline-topic-programs.repository.interface';
 import { Injectable, Logger } from '@nestjs/common';
-import { PrismaService } from '@tech-post-cast/database';
+import {
+  HeadlineTopicProgramWithQiitaPosts,
+  PrismaService,
+} from '@tech-post-cast/database';
 
 /**
  * IHeadlineTopicProgramsRepository の実装
@@ -16,10 +18,35 @@ export class HeadlineTopicProgramsRepository
   constructor(private readonly prisma: PrismaService) {}
 
   /**
+   * 指定IDのヘッドライントピック番組を取得する
+   * @param id ヘッドライントピック番組ID
+   * @returns ヘッドライントピック番組
+   */
+  async findById(
+    id: string,
+  ): Promise<HeadlineTopicProgramWithQiitaPosts | null> {
+    this.logger.debug(`HeadlineTopicProgramsRepository.findById called`, {
+      id,
+    });
+    const result = await this.prisma.headlineTopicProgram.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        posts: true,
+      },
+    });
+    this.logger.debug(`ヘッドライントピック番組を取得しました`, {
+      result,
+    });
+    return result;
+  }
+
+  /**
    * 最新のヘッドライントピック番組を取得する
    * @returns ヘッドライントピック番組
    */
-  async findLatest(): Promise<HeadlineTopicProgram | null> {
+  async findLatest(): Promise<HeadlineTopicProgramWithQiitaPosts | null> {
     this.logger.debug(`HeadlineTopicProgramsRepository.findLatest called`);
     const result = await this.prisma.headlineTopicProgram.findFirst({
       where: {
@@ -34,6 +61,9 @@ export class HeadlineTopicProgramsRepository
       },
       orderBy: {
         updatedAt: 'desc',
+      },
+      include: {
+        posts: true,
       },
     });
     this.logger.debug(`最新のヘッドライントピック番組を取得しました`, {
