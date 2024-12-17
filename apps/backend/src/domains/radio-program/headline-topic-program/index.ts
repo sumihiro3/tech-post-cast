8d03cf6a082fa6498f697a00219c186cffa28e20
sumihiro3 <sumihiro@gmail.com>
2024-12-17 17:17:22 +0900

    生成した番組音声・映像ファイルに metadata を付与した

--- apps/backend/src/domains/radio-program/headline-topic-program/index.ts
+++ apps/backend/src/domains/radio-program/headline-topic-program/index.ts
@@ -1,3 +1,5 @@
+import { formatDate } from '@tech-post-cast/commons';
+
 export * from './headline-topic-program-maker';
 
 /**
@@ -33,6 +35,59 @@ export interface HeadlineTopicProgramScript {
    * エンディング
    */
   ending: string;
+}
+
+/**
+ * ヘッドライントピック番組の情報を表すクラス
+ * ffmpeg で音声ファイル、動画ファイルに埋め込むメタデータ情報
+ */
+export class HeadlineTopicProgramInMetadata {
+  /** artist */
+  artist: string;
+  /** album */
+  album: string;
+  /** albumArtist */
+  albumArtist: string;
+  /** title */
+  title: string;
+  /** copyright */
+  copyright: string;
+  /** date */
+  date: string;
+  /** genre */
+  genre: string;
+  /** language */
+  language: string;
+  /** filename */
+  filename: string;
+
+  constructor(
+    script: HeadlineTopicProgramScript,
+    artistName: string,
+    programDate: Date,
+    fileName: string,
+  ) {
+    this.artist = artistName;
+    this.album = 'ヘッドライントピック';
+    this.albumArtist = artistName;
+    this.title = `${script.title}（${formatDate(programDate, 'YYYY-MM-DD')}）`;
+    this.date = formatDate(programDate, 'YYYY');
+    this.genre = 'Technology';
+    this.language = 'jpn';
+    this.filename = fileName;
+  }
+
+  // getFFmpegMetadataOptions(): string[][] {
+  //   return [
+  //     ['-metadata', `artist="${this.artist}"`],
+  //     ['-metadata', `album="${this.album}"`],
+  //     ['-metadata', `album_artist="${this.albumArtist}"`],
+  //     ['-metadata', `title="${this.title}"`],
+  //     ['-metadata', `date="${this.date}"`],
+  //     ['-metadata', `genre="${this.genre}"`],
+  //     ['-metadata', `language="${this.language}"`],
+  //   ];
+  // }
 }
 
 /**
