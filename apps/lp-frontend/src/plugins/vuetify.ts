9c30ac6f374e7f2954c26ff27bc372314adba9db
sumihiro3 <sumihiro@gmail.com>
2024-12-23 09:38:07 +0900

    Vuetify 用プラグインの追加

--- apps/lp-frontend/src/plugins/vuetify.ts
+++ apps/lp-frontend/src/plugins/vuetify.ts
@@ -1,12 +1,12 @@
 // import this after install `@mdi/font` package
-import '@mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css';
 
-import { createVuetify } from 'vuetify'
-import 'vuetify/styles'
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
     // ... your configuration
-  })
-  app.vueApp.use(vuetify)
-})
+  });
+  app.vueApp.use(vuetify);
+});
