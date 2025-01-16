f473d898440df384b055d241bf7a50a7bb079a89
sumihiro3 <sumihiro@gmail.com>
2025-01-16 09:40:56 +0900

    番組個別ページで台本を表示するようにした

--- apps/lp-frontend/src/components/content/headline-topic-program/index.vue
+++ apps/lp-frontend/src/components/content/headline-topic-program/index.vue
@@ -19,8 +19,9 @@ v-card.ma-1.pa-1.pa-md-2.mb-6.mb-md-10.bg-blue-grey-lighten-4(
     v-tab.text-none.text-grey-darken-1(value='posts')
       | 紹介記事
     v-tab.text-none.text-grey-darken-1(v-if='showScript', value='script')
-      | 台本
+      | 番組の台本
   v-tabs-window(v-model='tab')
+    //- 紹介記事一覧
     v-tabs-window-item(value='posts')
       ul.mt-4.ml-2.ml-md-6.post-list
         li.mb-2.text-body-2.text-md-body-1(
@@ -29,6 +30,17 @@ v-card.ma-1.pa-1.pa-md-2.mb-6.mb-md-10.bg-blue-grey-lighten-4(
         )
           a(:href='post.url', target='_blank')
             | {{ post.title }}
+    //- 番組の台本
+    v-tabs-window-item(v-if='showScript', value='script')
+      p.mt-4.ml-2.ml-md-6.text-body-2.text-md-body-1
+        | {{ program.script.intro }}
+      p.mt-4.ml-2.ml-md-6.text-body-2.text-md-body-1(
+        v-for='(post, index) in program.script.posts',
+        :key='index'
+      )
+        | {{ post.summary }}
+      p.mt-4.ml-2.ml-md-6.text-body-2.text-md-body-1
+        | {{ program.script.ending }}
 </template>
 
 <script lang="ts" setup>
