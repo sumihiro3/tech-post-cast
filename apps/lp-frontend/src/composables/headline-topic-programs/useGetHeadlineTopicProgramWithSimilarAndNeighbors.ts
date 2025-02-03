a4b2ab27576ebc087fa0213b080e6dcf77812180
sumihiro3 <sumihiro@gmail.com>
2025-02-03 23:42:22 +0900

    番組個別ページで類似番組を表示するようにした

---
+++ apps/lp-frontend/src/composables/headline-topic-programs/useGetHeadlineTopicProgramWithSimilarAndNeighbors.ts
@@ -0,0 +1,29 @@
+import type { NuxtApp } from '#app';
+import type { HeadlineTopicProgramWithSimilarAndNeighborsDto } from '@/api';
+
+/**
+ * 指定のヘッドライントピック番組と、その類似番組および、前後の日付の番組を取得する
+ * @param app Nuxtアプリケーション
+ * @param programId 番組ID
+ * @returns 指定のヘッドライントピック番組と、その類似番組および、前後の日付の番組
+ */
+export const useGetHeadlineTopicProgramWithSimilarAndNeighbors = async (
+  app: NuxtApp,
+  programId: string,
+): Promise<HeadlineTopicProgramWithSimilarAndNeighborsDto> => {
+  console.debug(`useGetHeadlineTopicProgramWithSimilarAndNeighbors called`, { programId });
+  const token = app.$config.public.apiAccessToken;
+  const bearerToken = `Bearer ${token}`;
+  const getProgramResponse = await app.$apiV1.getHeadlineTopicProgramWithSimilarAndNeighbors(
+    programId,
+    bearerToken,
+  );
+  const dto = getProgramResponse.data;
+  console.log(`ヘッドライントピック番組`, {
+    similar: dto.similar.map(s => ({ id: s.id, title: s.title, createdAt: s.createdAt })),
+    previous: { id: dto.previous?.id, title: dto.previous?.title, createdAt: dto.previous?.createdAt },
+    target: { id: dto.target.id, title: dto.target.title, createdAt: dto.target.createdAt },
+    next: { id: dto.next?.id, title: dto.next?.title, createdAt: dto.next?.createdAt },
+  });
+  return dto;
+};
--- apps/lp-frontend/src/pages/headline-topic-programs/[id].vue
+++ apps/lp-frontend/src/pages/headline-topic-programs/[id].vue
@@ -1,5 +1,6 @@
 <template lang="pug">
 div(v-if='result && result.target')
+  //- 表示対象のヘッドライントピック番組
   v-row(align='center')
     v-col(cols='0', sm='2').mb-0.pb-0
     v-col(cols='12', sm='8').mb-0.pb-0
@@ -9,45 +10,60 @@ div(v-if='result && result.target')
         :showScript='true'
       )
     v-col(cols='0', sm='2').mb-0.pb-0
-  v-row(align='center')
-    v-col(cols='0', sm='2').mb-0.pb-0
-    v-col(cols='12', sm='8').mb-0.pb-0
-      div.text-center.text-md-left.text-subtitle-1.font-weight-bold
-        | 前後の番組
-    v-col(cols='0', sm='2').mb-0.pb-0
-  v-row(align='center').px-0.mx-0
-    v-col(cols='0', sm='2').mb-0.pb-0
-    v-col(cols='12', sm='4').mb-0.pb-0
-      v-row.d-flex.align-center.justify-center.fill-height
-        v-col(cols='1').mb-0.pb-0
-          v-icon(v-if="result.next" color='primary', size='64' icon="mdi-chevron-left").pr-12.pb-4
-        v-col(cols='11').mb-0.pb-0
-          ContentHeadlineTopicProgramSmall(v-if="result.next" :key='result.next?.id', :program='result.next')
-    v-col(cols='12', sm='4').mb-0.pb-0
-      v-row.d-flex.align-center.justify-center.fill-height
-        v-col(cols='11').mb-0.pb-0
-          ContentHeadlineTopicProgramSmall(v-if="result.previous" :key='result.previous?.id', :program='result.previous')
-        v-col(cols='1').mb-0.pb-0
-          v-icon(v-if="result.previous" color='primary', size='64' icon="mdi-chevron-right").pr-12.pb-4
-    v-col(cols='0', sm='2').mb-0.pb-0
+  //- 類似番組
+  div(v-if="result.similar").pa-0.ma-0
+    v-row(align='center')
+      v-col(cols='0', sm='2').mb-0.pb-0
+      v-col(cols='12', sm='8').mb-0.pb-0
+        div.text-center.text-md-left.text-subtitle-1.font-weight-bold
+          | よく似た番組
+      v-col(cols='0', sm='2').ma-0.pa-0
+    v-row(align='center').px-0.mx-0
+      v-col(cols='0', sm='2').ma-0.pa-0
+      v-col(cols='12', sm='8').ma-0.pa-0
+        ContentHeadlineTopicProgramSmall(v-for="program in result.similar", :key='program.id', :program='program')
+      v-col(cols='0', sm='2').ma-0.pa-0
+  //- 前後の番組
+  div().pa-0.ma-0
+    v-row(align='center')
+      v-col(cols='0', sm='2').mb-0.pb-0
+      v-col(cols='12', sm='8').mb-0.pb-0
+        div.text-center.text-md-left.text-subtitle-1.font-weight-bold
+          | 前後の番組
+      v-col(cols='0', sm='2').ma-0.pa-0
+    v-row(align='center')
+      v-col(cols='0', sm='2').mb-0.pb-0
+      v-col(cols='12', sm='4').mb-0.pb-0
+        v-row.d-flex.align-center.justify-center.fill-height
+          v-col(cols='1').mb-0.pb-0
+            v-icon(v-if="result.next" color='primary', size='64' icon="mdi-chevron-left").pr-12.pb-4
+          v-col(cols='11').mb-0.pb-0
+            ContentHeadlineTopicProgramSmall(v-if="result.next" :key='result.next?.id', :program='result.next')
+      v-col(cols='12', sm='4').mb-0.pb-0
+        v-row.d-flex.align-center.justify-center.fill-height
+          v-col(cols='11').mb-0.pb-0
+            ContentHeadlineTopicProgramSmall(v-if="result.previous" :key='result.previous?.id', :program='result.previous')
+          v-col(cols='1').mb-0.pb-0
+            v-icon(v-if="result.previous" color='primary', size='64' icon="mdi-chevron-right").pr-12.pb-4
+      v-col(cols='0', sm='2').mb-0.pb-0
 </template>
 
 <script setup lang="ts">
-import type { HeadlineTopicProgramWithNeighborsDto } from '@/api';
-import { useGetHeadlineTopicProgramWithNeighbors } from '@/composables/headline-topic-programs/useGetHeadlineTopicProgramWithNeighbors';
+import type { HeadlineTopicProgramWithSimilarAndNeighborsDto } from '@/api';
+import { useGetHeadlineTopicProgramWithSimilarAndNeighbors } from '@/composables/headline-topic-programs/useGetHeadlineTopicProgramWithSimilarAndNeighbors';
 
 const app = useNuxtApp();
 const route = useRoute();
 const { id } = route.params;
 const programId = Array.isArray(id) ? id[0] : id;
 
-const { data: result } = await useAsyncData<HeadlineTopicProgramWithNeighborsDto>(
+const { data: result } = await useAsyncData<HeadlineTopicProgramWithSimilarAndNeighborsDto>(
   `headline-topic-program:${programId}`,
   async () => {
     try {
       // 指定されたIDの番組情報と、前後の日付の番組情報を取得
-      const dto = await useGetHeadlineTopicProgramWithNeighbors(app, programId);
-      return { target: dto.target, previous: dto.previous, next: dto.next };
+      const dto = await useGetHeadlineTopicProgramWithSimilarAndNeighbors(app, programId);
+      return { target: dto.target, similar: dto.similar, previous: dto.previous, next: dto.next };
     }
     catch (error) {
       console.error(`番組 [${programId}] の取得に失敗しました。`, error);
