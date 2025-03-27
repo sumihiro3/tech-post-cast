de3bb5db4c2f9e2d6c24f808273f2a74b43be0c0
sumihiro3 <sumihiro@gmail.com>
2025-03-27 20:45:31 +0900

    Clerk の Webhook から AppUser を登録、更新、削除する処理を追加した

--- apps/api-backend/src/controllers/webhooks/clerk/clerk-webhook.module.ts
+++ apps/api-backend/src/controllers/webhooks/clerk/clerk-webhook.module.ts
@@ -1,3 +1,4 @@
+import { AppUserRepository } from '@infrastructure/database/app-users/app-user.repository';
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '@tech-post-cast/database';
 import { ClerkWebhookController } from './clerk-webhook.controller';
@@ -6,6 +7,12 @@ import { ClerkWebhookService } from './clerk-webhook.service';
 @Module({
   imports: [PrismaModule],
   controllers: [ClerkWebhookController],
-  providers: [ClerkWebhookService],
+  providers: [
+    ClerkWebhookService,
+    {
+      provide: 'AppUserRepository',
+      useClass: AppUserRepository,
+    },
+  ],
 })
 export class ClerkWebhookModule {}
