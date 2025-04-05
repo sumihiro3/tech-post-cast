debba765a0357c4a32a970f14f5d52f479dc5838
sumihiro3 <sumihiro@gmail.com>
2025-04-05 11:05:21 +0900

    ESLint 設定を変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,8 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'error',
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@stylistic/arrow-parens': 'off',
+    '@stylistic/operator-linebreak': 'off',
     'import/order': [
       'error',
       {
