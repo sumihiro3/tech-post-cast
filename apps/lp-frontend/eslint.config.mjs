52a156dcf3e78caee92e4cdd85089fb27789a116
sumihiro3 <sumihiro@gmail.com>
2025-04-08 09:56:19 +0900

    設定ファイルを更新し、ESLintの自動修正設定を変更した

--- apps/lp-frontend/eslint.config.mjs
+++ apps/lp-frontend/eslint.config.mjs
@@ -16,6 +16,15 @@ export default withNuxt([
   {
     ignores: ['api/**/*.ts', 'api/**/*.js', 'api/**/*.vue'],
   },
+  stylistic.configs.customize({
+    flat: true,
+    indent: 2,
+    quotes: 'single',
+    semi: true,
+    arrowParens: false,
+    operatorLinebreak: 'none',
+    braceStyle: false,
+  }),
   ...eslintrc.extends('plugin:vue-pug/vue3-recommended'),
   {
     files: ['**/*.vue'],
@@ -36,15 +45,8 @@ export default withNuxt([
       '@typescript-eslint/explicit-function-return-type': 'error',
       '@stylistic/arrow-parens': 'off',
       '@stylistic/operator-linebreak': 'off',
+      '@stylistic/brace-style': 'off',
       ...stylisticRules,
     },
   },
-  stylistic.configs.customize({
-    flat: true,
-    indent: 2,
-    quotes: 'single',
-    semi: true,
-    arrowParens: false,
-    operatorLinebreak: 'none',
-  }),
 ]);
