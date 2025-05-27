30b035caf3c858ee3067ac901ea46c6b62ff2b9b
sumihiro3 <sumihiro@gmail.com>
2025-05-27 12:11:31 +0900

    ユーザー設定のインフラ層を実装

--- apps/api-backend/src/types/errors/index.ts
+++ apps/api-backend/src/types/errors/index.ts
@@ -3,3 +3,4 @@ export * from './headline-topic-program.error';
 export * from './personalized-feed.error';
 export * from './personalized-program.error';
 export * from './subscription.error';
+export * from './user-settings.error';
