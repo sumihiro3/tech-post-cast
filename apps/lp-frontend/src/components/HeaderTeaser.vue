d8482a60535e1eb224887f7b19182a656e9af318
sumihiro3 <sumihiro@gmail.com>
2025-06-04 22:34:47 +0900

    パーソナルフィードのティザーサイトを追加

---
+++ apps/lp-frontend/src/components/HeaderTeaser.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+v-container(fluid)
+  v-app-bar
+    template(#prepend)
+      a(href='/')
+        logo
+      v-app-bar-nav-icon.ml-2(
+        v-if="props.showDrawerToggle"
+        @click="emit('toggle-drawer')"
+      )
+
+</template>
+
+<script setup lang="ts">
+// プロパティ定義
+const props = defineProps({
+  showDrawerToggle: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// ドロワー切り替えイベントを定義
+const emit = defineEmits(['toggle-drawer']);
+</script>
